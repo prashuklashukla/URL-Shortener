b8f88e067006e712ef2075e0ff284c1433aa0cda
prashant shukla <ankurshukla051@gmail.com>
2026-03-11 00:01:54 +0530

    leftBig

---
+++ src/config/db.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose')
+
+async function connectDB() {
+    try {
+        await mongoose.connect(process.env.MONGO_URL)
+        console.log('DB connect')
+    } catch (error) {
+        console.log(error)
+        console.log('DB error')
+        process.exit(1)
+    }
+}
